e71617772f0ffabe6e73865fc2c4a470570eb346
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-20 00:59:07 +0100

    *edit* front / adding new dashboard styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,18 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                oswald: ['Oswald', ...defaultTheme.fontFamily.sans],
+                jost: ['Jost', ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 'dash-bg': 'rgb(var(--color-dash-bg) / <alpha-value>)',
                 'dashcomponent-bg': 'rgb(var(--color-dashcomponent-bg) / <alpha-value>)',
                 'dashcomponent-border': 'rgb(var(--color-dashcomponent-border) / <alpha-value>)',
+                'activemenuitem-bg' : 'rgb(var(--color-activemenuitem-bg) / <alpha-value>)',
+                'offwhite' : 'rgb(var(--color-offwhite-font) / <alpha-value>)',
+                'offblack' : 'rgb(var(--color-offblack-font) / <alpha-value>)',
+                'tablebutton-bg' : 'rgb(var(--color-tablebutton-bg) / <alpha-value>)',
+                'redtablebutton-color' : 'rgb(var(--color-redtablebutton) / <alpha-value>)',
             },
         },
     },
